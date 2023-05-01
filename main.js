bac8788b45faede5be94d6aa91fa119988bf4fd8
FanyDmes <95221534+FanyDmes@users.noreply.github.com>
2023-05-01 10:40:37 -0300

    commit

---
+++ main.js
@@ -0,0 +1,61 @@
+const app = document.getElementById('typewriter');
+
+const typewriter = new Typewriter(app, {
+    loop: true,
+    delay: 150
+});
+
+typewriter
+    .typeString('LOS ESPERAMOS!!')
+    .pauseFor(1500)
+    .start();
+
+const color_fondo = document.getElementById('color_fondo')
+const numero = document.getElementById('numero')
+let cantidad = 0
+
+let tiempo = setInterval(() => {
+    cantidad += 50
+    color_fondo.style.height = `${cantidad}%`
+    numero.textContent = cantidad
+    if (cantidad === 12000) {
+        clearInterval(tiempo)
+    }
+}, 33);
+
+const fondo_color = document.getElementById('fondo_color')
+const number = document.getElementById('number')
+let cant = 0
+
+let time = setInterval(() => {
+    cant += 10
+    fondo_color.style.height = `${cant}%`
+    number.textContent = cant
+    if (cant === 1000) {
+        clearInterval(time)
+    }
+}, 80);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
